93fc0d5a83c5338cf33bb5e6e6af672bae025f04
lucas <magroski@gmail.com>
2018-02-06 21:37:23 -0200

    moving data to json

--- public/js/savePoints.js
+++ public/js/savePoints.js
@@ -5,22 +5,18 @@ var t = TrelloPowerUp.iframe();
 // Elements with IDs are available as properties of `window`.
 window.pointsForm.addEventListener('submit', function(event){
     // Stop the browser trying to submit the form itself.
-    event.preventDefault();
-    t.set('card', 'shared', 'estimated', window.estimated.value);
-    return t.set('card', 'shared', 'consumed', window.consumed.value)
+    event.preventDefault();    
+    return t.set('card', 'shared', 'agilePoints', {estimated: window.estimated.value, consumed:window.consumed.value})
     .then(function(){
         t.closePopup();  
     });    
 });
 
 t.render(function(){
-    t.get('card', 'shared', 'estimated').
-    then(function(estimated){
-        window.estimated.value = estimated;
-    });
-  return t.get('card', 'shared', 'consumed')
-  .then(function(consumed){
-        window.consumed.value = consumed;
+  return t.get('card', 'shared', 'agilePoints')
+  .then(function(data){
+        window.estimated.value = data.estimated;
+        window.consumed.value = data.consumed;
   })
   .then(function(){
     t.sizeTo('#pointsForm').done();
