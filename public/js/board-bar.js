8e9fe079a483d4f8084127344d0f017b3b5ae97d
magroski <magroski@gmail.com>
2018-02-07 09:38:43 -0200

    testing

--- public/js/board-bar.js
+++ public/js/board-bar.js
@@ -5,6 +5,17 @@ var t = TrelloPowerUp.iframe();
 t.render(function () {
     return t.lists('all')
             .then(function (lists) {
+                var listsSummary = [];
+                for(var i = 0; i < lists.length; i++){
+                    var currentList = lists[i];
+                    var listName = currentList.name;
+                    var estimatedPoints = 0;
+                    var consumedPoints = 0;
+                    for(var j = 0; j < currentList.cards.length; j++){
+                        var currentCard = currentList.cards[j];
+                        console.log(t.get(currentCard.id, 'shared', 'agilePoints'));
+                    }
+                }
                 console.log(JSON.stringify(lists, null, 2));
             });
 });
