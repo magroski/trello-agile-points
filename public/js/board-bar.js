64a39d2878262ab9760aeb3130bf1e39e3f01a88
magroski <magroski@gmail.com>
2018-02-07 10:02:36 -0200

    test

--- public/js/board-bar.js
+++ public/js/board-bar.js
@@ -6,16 +6,30 @@ t.render(function () {
     return t.lists('all')
             .then(function (lists) {
                 var listsSummary = [];
-                for(var i = 0; i < lists.length; i++){
+                for (var i = 0; i < lists.length; i++) {
                     var currentList = lists[i];
-                    var listName = currentList.name;
-                    var estimatedPoints = 0;
-                    var consumedPoints = 0;
-                    for(var j = 0; j < currentList.cards.length; j++){
+                    var listName    = currentList.name;
+                    var listObject  = {
+                        name     : listName,
+                        estimated: 0,
+                        consumed : 0
+                    };
+                    for (var j = 0; j < currentList.cards.length; j++) {
                         var currentCard = currentList.cards[j];
-                        console.log(t.get(currentCard.id, 'shared', 'agilePoints'));
+                        t.get(currentCard.id, 'shared', 'agilePoints')
+                         .then(function (listObject, agilePoints) {
+                             if (!isNaN(agilePoints.consumed)) {
+                                 listObject.consumed += agilePoints.consumed;
+                             }
+                             if (!isNaN(agilePoints.estimated)) {
+                                 listObject.estimated += agilePoints.estimated;
+                             }
+                             return true;
+                         }.bind(null, listObject));
                     }
+                    listsSummary.push(listObject);
                 }
                 console.log(JSON.stringify(lists, null, 2));
+                console.log(listsSummary);
             });
 });
