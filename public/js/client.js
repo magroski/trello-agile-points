2fd0515e9de84a97ed562f55956ba42433a58de0
magroski <magroski@gmail.com>
2018-02-07 16:05:10 -0200

    svg icon

--- public/js/client.js
+++ public/js/client.js
@@ -2,7 +2,7 @@
 
 var Promise = TrelloPowerUp.Promise;
 
-var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
+var BLACK_ROCKET_ICON = './i/card.svg';
 
 var calculateSprintPoints = function (t, options) {
     return t.boardBar({
