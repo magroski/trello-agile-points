e0520b11cad2f82c5f346aea6905893af85c694d
lucas <magroski@gmail.com>
2018-02-06 22:06:25 -0200

    testing board bar

--- public/js/client.js
+++ public/js/client.js
@@ -4,6 +4,13 @@ var Promise = TrelloPowerUp.Promise;
 
 var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
 
+var calculateSprintPoints = function(t){
+    t.boardBar({
+        height: 200,
+        url: './board-bar.html'
+    });
+}
+
 TrelloPowerUp.initialize({
     'card-buttons': function(t, options) {
         return [{
@@ -26,4 +33,11 @@ TrelloPowerUp.initialize({
             }];
         });
     },
+    'board-buttons': function(t, options){
+        return [{
+            icon: BLACK_ROCKET_ICON,
+            text: 'Show sprint points',
+            callback: calculateSprintPoints(t)
+        }];
+    }
 });
